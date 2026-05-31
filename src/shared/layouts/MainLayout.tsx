6d02add8870bf3a627185ff8bd1c2cf865ed19ca
Jefferson Lizarazu <Jeffersonlizarazu@hotmail.com>
2026-05-30 21:12:13 -0500

    feat: add dark mode toggle with CSS variables and theme infrastructure

--- src/shared/layouts/MainLayout.tsx
+++ src/shared/layouts/MainLayout.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from 'react-router-dom';
+import { useDarkMode } from '../hooks/useDarkMode';
 import { Navbar } from './Navbar';
 import { Footer } from './Footer';
 
 export function MainLayout() {
+  const { isDark, toggle } = useDarkMode();
+
   return (
     <>
-      <Navbar />
+      <Navbar isDark={isDark} onToggleDark={toggle} />
       <main className="flex-1">
         <Outlet />
       </main>
